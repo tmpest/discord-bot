0f817512ce673b558856b61099a17d3715db4a64
Josh <jharris4491@gmail.com>
2019-12-04 22:15:57 -0800

    missed change

--- pkg/server/endpoints.go
+++ pkg/server/endpoints.go
@@ -2,6 +2,6 @@ package server
 
 const discordOAuth2TokenEndpoint = "https://discordapp.com/api/v6/oauth2/token"
 
-const redirectURI = "https://tmpest-bot-server.herokuapp.com/"
+const redirectURI = "https://tmpest-bot-serveqr.herokuapp.com/"
 
 // const redirectURI = "http://localhost:44910/"
