ec50a27920d14786d728c5760bb4832a4dc152ce
Josh <jharris4491@gmail.com>
2019-12-04 22:13:00 -0800

    i think this works

--- pkg/server/endpoints.go
+++ pkg/server/endpoints.go
@@ -1,4 +1,7 @@
 package server
 
 const discordOAuth2TokenEndpoint = "https://discordapp.com/api/v6/oauth2/token"
+
 const redirectURI = "https://tmpest-bot-server.herokuapp.com/"
+
+// const redirectURI = "http://localhost:44910/"
